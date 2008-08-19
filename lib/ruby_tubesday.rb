70ec71c4bac6abd71545800b6df167a0c997367b
Dana Contreras <dana@danadanger.org>
2008-08-19 14:40:11 -0400

    added docs

--- lib/ruby_tubesday.rb
+++ lib/ruby_tubesday.rb
@@ -4,9 +4,57 @@ require 'net/https'
 require 'rubygems'
 require 'activesupport'
 
+require 'ruby_tubesday/parser'
+require 'ruby_tubesday/cache_policy'
+
+# RubyTubesday is a full-featured HTTP client. It supports automatic parsing
+# of content bodies, caching, redirection, verified SSL, and basic
+# authentication.
+#
+# If you have the json gem, RubyTubesday will automatically parse JSON
+# responses. You can add parsers for other content types with the
+# RubyTubesday::Parser class.
+#
+# == Example
+#
+#   require 'rubygems'
+#   require 'ruby_tubesday'
+#   
+#   http = RubyTubesday.new :params => { :api_key => '12345', :output => 'json' }
+#   result = http.get 'http://maps.google.com/maps/geo', :params => { :q => '123 Main Street' }
+#   lat_lng = result['Placemark'].first['Point']['coordinates']
+#
 class RubyTubesday
+	# Thrown when a request is redirected too many times.
 	class TooManyRedirects < Exception; end
 	
+	# Creates a new HTTP client. Accepts the following options:
+	#
+	# raw::             Whether to always return raw content bodies. If this is
+	#                   false, content types registered with RubyTubesday::Parser
+	#                   will be automatically parsed for you. Default is false.
+	# cache::           An instance of an ActiveSupport::Cache subclass to use
+	#                   as a content cache. You can set this to false to disable
+	#                   caching. Default is a new MemoryStore.
+	# params::          Parameters to include in every request. For example, if
+	#                   the service you're accessing requires an API key, you can
+	#                   set it here and it will be included in every request made
+	#                   from this client.
+	# max_redirects::   Maximum number of redirects to follow in a single request
+	#                   before throwing an exception. Default is five.
+	# ca_file::         Path to a file containing certifying authority
+	#                   certificates (for verifying SSL server certificates).
+	#                   Default is the CA bundle included with RubyTubesday,
+	#                   which is a copy of the bundle included with CentOS 5.
+	# verify_ssl::      Whether to verify SSL certificates. If a certificate
+	#                   fails verification, the request will throw an exception.
+	#                   Default is true.
+	# username::        Username to send using basic authentication with every
+	#                   request.
+	# password::        Username to send using basic authentication with every
+	#                   request.
+	#
+	# All of these options can be overriden on a per-request basis.
 	def initialize(options = {})
 		@default_options = {
 			:raw           => false,
@@ -21,6 +69,19 @@ class RubyTubesday
 		@default_options = normalize_options(options)
 	end
 	
+	# Fetches a URL using the GET method. Accepts the same options as new.
+	# Options specified here are merged into the options specified when the
+	# client was instantiated.
+	#
+	# Parameters in the URL will be merged with the params option. The params
+	# option supercedes parameters specified in the URL. For example:
+	#
+	#   # Fetches http://example.com/search?q=ruby&lang=en
+	#   http.get 'http://example.com/search?q=ruby', :params => { :lang => 'en' }
+	#
+	#   # Fetches http://example.com/search?q=ruby&lang=ja
+	#   http.get 'http://example.com/search?q=ruby&lang=en', :params => { :lang => 'ja' }	
+	#
 	def get(url, options = {})
 		options = normalize_options(options)
 		url = URI.parse(url)
@@ -44,6 +105,14 @@ class RubyTubesday
 		process_request(request, url, options)
 	end
 	
+	# Sends data to a URL using the POST method. Accepts the same options as new.
+	# Options specified here are merged into the options specified when the
+	# client was instantiated.
+	#
+	# Parameters in the URL will be ignored. The post body will be URL-encoded.
+	#
+	# This method never uses the cache.
+	#
 	def post(url, options = {})
 		options = normalize_options(options)
 		url = URI.parse(url)
@@ -56,7 +125,7 @@ class RubyTubesday
   
 protected
 	
-	def normalize_options(options)
+	def normalize_options(options) # :nodoc:
 		normalized_options = {
 			:raw           => options.delete(:raw),
 			:cache         => options.delete(:cache),
@@ -79,10 +148,11 @@ protected
     normalized_options
 	end
 	
-	def process_request(request, url, options)
+	def process_request(request, url, options) # :nodoc:
 		response = nil
 		cache_policy_options = CachePolicy.options_for_cache(options[:cache]) || {}
 		
+		# Check the cache first if this is a GET request.
 		if request.is_a?(Net::HTTP::Get) && options[:cache] && options[:cache].read(url.to_s)
 			response = Marshal.load(options[:cache].read(url.to_s))
 			response_age = Time.now - Time.parse(response['Last-Modified'] || response['Date'])
@@ -92,18 +162,25 @@ protected
 			end
 		end
 		
+		# Cache miss. Fetch the entity from the network.
 		if response.nil?
 			redirects_left = options[:max_redirects]
 			
 			while !response.is_a?(Net::HTTPSuccess)
+				client = Net::HTTP.new(url.host, url.port)
+				
+				# Configure authentication.
 				if options[:username] && options[:password]
 					request.basic_auth options[:username], options[:password]
 				end
-				client = Net::HTTP.new(url.host, url.port)
+				
+				# Configure SSL.
 				if (client.use_ssl = url.is_a?(URI::HTTPS))
 					client.verify_mode = options[:verify_ssl] ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
 					client.ca_file = options[:ca_file]
 				end
+				
+				# Send the request.
 				response = client.start { |w| w.request(request) }
 				
 				if response.is_a?(Net::HTTPRedirection)
@@ -116,6 +193,7 @@ protected
 				end
 			end
 			
+			# Write the response to the cache if we're allowed to.
 			if request.is_a?(Net::HTTP::Get) && options[:cache]
 				cache_policy = CachePolicy.new(response['Cache-Control'], cache_policy_options)
 				if cache_policy.may_cache?
@@ -124,6 +202,7 @@ protected
 			end
 		end
     
+    # Return the response.
     if options[:raw]
 	    response.body
 	  else
@@ -131,6 +210,3 @@ protected
 	  end
   end
 end
-
-require 'ruby_tubesday/parser'
-require 'ruby_tubesday/cache_policy'
--- ruby_tubesday.gemspec
+++ ruby_tubesday.gemspec
@@ -1,10 +1,15 @@
 Gem::Specification.new do |spec|
 	spec.name = 'ruby_tubesday'
-	spec.version = '1.0'
+	spec.version = '0.1'
 	spec.summary = 'Full-featured HTTP client library.'
 	spec.author = 'DanaDanger'
 	spec.homepage = 'http://github.com/DanaDanger/ruby_tubesday'
 	spec.has_rdoc = true
-	spec.files = Dir.glob('lib/**/*.rb', 'ca-bundle.crt')
+	spec.files = [
+		'lib/ruby_tubesday.rb',
+		'lib/ruby_tubesday/cache_policy.rb',
+		'lib/ruby_tubesday/parser.rb',
+		'ca-bundle.crt'
+	]
 	spec.add_dependency('activesupport', '>=2.1')
 end
